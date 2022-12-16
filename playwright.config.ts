e6e16afa7dc790e47338bf9e031b16ff1268bdbd
Michael Zuercher <michzuerch@gmail.com>
2022-12-16 13:26:20 +0100

    first working tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
 	testDir: './tests',
 	/* Maximum time one test can run for. */
 	timeout: 30 * 1000,
 	expect: {
-		/**
-		 * Maximum time expect() should wait for the condition to be met.
-		 * For example in `await expect(locator).toHaveText();`
-		 */
 		timeout: 5000,
 	},
 	/* Run tests in files in parallel */
@@ -33,7 +20,7 @@ const config: PlaywrightTestConfig = {
 	reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		headless: false,
+		headless: true,
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -59,50 +46,13 @@ const config: PlaywrightTestConfig = {
 			},
 		},
 
-		// {
-		// 	name: 'webkit',
-		// 	use: {
-		// 		...devices['Desktop Safari'],
-		// 	},
-		// },
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: {
-		//     ...devices['Pixel 5'],
-		//   },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: {
-		//     ...devices['iPhone 12'],
-		//   },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: {
-		//     channel: 'msedge',
-		//   },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: {
-		//     channel: 'chrome',
-		//   },
-		// },
+		{
+			name: 'Mobile Chrome',
+			use: {
+				...devices['Pixel 5'],
+			},
+		},
 	],
-
-	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
-	// outputDir: 'test-results/',
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   port: 3000,
-	// },
 };
 
 export default config;
---
+++ tests/login.test.ts
@@ -0,0 +1,12 @@
+import { chromium, test } from '@playwright/test';
+
+test('Login test demo', async () => {
+	const browser = await chromium.launch();
+	const context = await browser.newContext();
+	const page = await context.newPage();
+
+	//await page.goto('https://michzuerch.github.io/');
+	await page.goto('https://ecommerce-playground.lambdatest.io/');
+	await page.hover("//a[@data-toggle='drop-down']//span[contains(.,'My account')]");
+	await page.click('test=Login');
+});
