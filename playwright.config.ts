86a5d1b3e5c47a528ffd9b016f68bb39cbf1de49
Michael Zuercher <michzuerch@gmail.com>
2022-12-22 15:46:05 +0100

    Single browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const config: PlaywrightTestConfig = {
 	use: {
 		actionTimeout: 30 * 1000,
 		headless: true,
-		locale: 'de-de',
+		locale: 'de-DE',
 		baseURL: 'http://localhost:3000/PrivaterAutomarkt7/',
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		ignoreHTTPSErrors: true,
@@ -22,7 +22,7 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run dev',
 		url: 'http://localhost:3000/PrivaterAutomarkt7/',
 		timeout: 30 * 1000,
-		reuseExistingServer: !process.env.CI
+		reuseExistingServer: true
 	}
 
 	/*
