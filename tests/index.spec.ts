07eab0f01dd87b7b84d681f5524ce9e8aaafb0ae
Michael Zuercher <michzuerch@gmail.com>
2023-01-13 14:55:57 +0100

    Lighthouse test skipped on other browsers

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,4 @@
-import { test, expect, chromium } from '@playwright/test'
-import { playAudit } from 'playwright-lighthouse'
-import { devices } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
 	await page.goto('http://localhost:3000/PrivaterAutomarkt7/')
@@ -35,70 +33,3 @@ test.describe('Links from index', () => {
 	})
 })
 
-const lighthouseConfig: PlaywrightTestConfig = {
-	testDir: './tests',
-	timeout: 60 * 1000,
-	fullyParallel: true,
-	workers: 4,
-	reporter: [['html', { open: 'never' }], ['list']],
-	use: {
-		actionTimeout: 30 * 1000,
-		headless: true,
-		locale: 'de-DE',
-		baseURL: 'http://localhost:3000/PrivaterAutomarkt7/',
-		viewport: { width: 600, height: 900 },
-		ignoreHTTPSErrors: true,
-		video: 'on',
-		trace: 'on'
-	},
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] }
-		},
-	],
-
-	webServer: {
-		command: 'npm run dev',
-		url: 'http://localhost:3000/PrivaterAutomarkt7/',
-		timeout: 30 * 1000,
-		reuseExistingServer: true
-	}
-}
-
-test.describe('Lighthouse score', () => {
-	test.use(lighthouseConfig)
-	test(`Lighthouse performance test`, async () => {
-		const browser = await chromium.launch({
-			args: ['--remote-debugging-port=9222'],
-			headless: true
-		})
-		const page = await browser.newPage()
-		await page.goto('http://localhost:3000/PrivaterAutomarkt7/')
-		await playAudit({
-			page: page,
-			thresholds: {
-				performance: 80,
-				accessibility: 50,
-				'best-practices': 50,
-				seo: 50
-			},
-			port: 9222,
-			ignoreError: true,
-
-			//opts: options,
-			reports: {
-				formats: {
-					html: true,
-					csv: false,
-					json: false
-				},
-				//name: `ligthouse-${new Date().toISOString()}`, //defaults to `lighthouse-${new Date().getTime()}`
-				name: `ligthouse`, //defaults to `lighthouse-${new Date().getTime()}`
-				directory: `${process.cwd()}/lighthouse` //defaults to `${process.cwd()}/lighthouse`
-			}
-		})
-		await page.close()
-		await browser.close()
-	})
-})
---
+++ tests/lighthouse.spec.ts
@@ -0,0 +1,46 @@
+
+import { test, expect, chromium } from '@playwright/test'
+import { playAudit } from 'playwright-lighthouse'
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('http://localhost:3000/PrivaterAutomarkt7/')
+	//await page.waitForLoadState('networkidle');
+})
+
+test.describe('Lighthouse score', () => {
+	test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!')
+
+	test('Lighthouse score', async () => {
+		const browser = await chromium.launch({
+			args: ['--remote-debugging-port=9222'],
+			headless: true
+		})
+		const page = await browser.newPage()
+		await page.goto('http://localhost:3000/PrivaterAutomarkt7/')
+		await playAudit({
+			page: page,
+			thresholds: {
+				performance: 80,
+				accessibility: 50,
+				'best-practices': 50,
+				seo: 50
+			},
+			port: 9222,
+			ignoreError: true,
+
+			//opts: options,
+			reports: {
+				formats: {
+					html: true,
+					csv: false,
+					json: false
+				},
+				//name: `ligthouse-${new Date().toISOString()}`, //defaults to `lighthouse-${new Date().getTime()}`
+				name: `ligthouse`, //defaults to `lighthouse-${new Date().getTime()}`
+				directory: `${process.cwd()}/lighthouse` //defaults to `${process.cwd()}/lighthouse`
+			}
+		})
+		await page.close()
+		await browser.close()
+	})
+})
