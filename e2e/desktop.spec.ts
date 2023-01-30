19ebb0db6d18a4920eeb8d42da9fae7e1a2b8b7a
Michael Zuercher <michzuerch@gmail.com>
2023-01-30 12:01:55 +0100

    test with screenshots

--- e2e/desktop.spec.ts
+++ e2e/desktop.spec.ts
@@ -1,39 +1,54 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Basics', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
-		await page.waitForLoadState('networkidle');
-	});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
+    await page.waitForLoadState('networkidle');
+  });
 
-	test('Page title is correct', async ({ page }) => {
-		await expect(page).toHaveTitle('Privater Automarkt Radolfzell');
-	});
+  test.afterEach(async ({ page }) => {
+    page.close;
+  });
+
+  test('Page title is correct', async ({ page }) => {
+    await expect(page).toHaveTitle('Privater Automarkt Radolfzell');
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
+});
+
+test.describe('Back to home', () => {
+  test('Go to location and test link to home', async ({ page }) => {
+    await page.goto('http://localhost:3000/PrivaterAutomarkt7/location/');
+    await page.waitForLoadState('networkidle');
+  });
 });
 
 test.describe('Links from index', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
-		await page.waitForLoadState('networkidle');
-	});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
+    await page.waitForLoadState('networkidle');
+  });
+  test('gallery desktop', async ({ page }) => {
+    const getGallery = page.getByRole('link', { name: 'Galerie' });
+    await expect(getGallery).toHaveAttribute('href', '/PrivaterAutomarkt7/gallery/');
+    await getGallery.click();
+    await expect(page).toHaveURL(/.*gallery/);
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
 
-	test('Check link/navigation for impressum', async ({ page }) => {
-		const getImpressum = page.getByRole('link', { name: 'Impressum' });
-		await expect(getImpressum).toHaveAttribute('href', '/PrivaterAutomarkt7/impressum/');
-		await getImpressum.click();
-		await expect(page).toHaveURL(/.*impressum/);
-	});
-	test('Check link/navigation for location', async ({ page }) => {
-		const getLocation = page.getByRole('link', { name: 'Standort' });
-		await expect(getLocation).toHaveAttribute('href', '/PrivaterAutomarkt7/location/');
-		await getLocation.click();
-		await expect(page).toHaveURL(/.*location/);
-		//await page.screenshot({ path: 'location.png', fullPage: true })
-	});
-	test('Check link/navigation for gallery', async ({ page }) => {
-		const getGallery = page.getByRole('link', { name: 'Galerie' });
-		await expect(getGallery).toHaveAttribute('href', '/PrivaterAutomarkt7/gallery/');
-		await getGallery.click();
-		await expect(page).toHaveURL(/.*gallery/);
-	});
+  test('location desktop', async ({ page }) => {
+    const getLocation = page.getByRole('link', { name: 'Standort' });
+    await expect(getLocation).toHaveAttribute('href', '/PrivaterAutomarkt7/location/');
+    await getLocation.click();
+    await expect(page).toHaveURL(/.*location/);
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true, timeout: 10 * 1000 });
+  });
+
+  test('impressum desktop', async ({ page }) => {
+    const getImpressum = page.getByRole('link', { name: 'Impressum' });
+    await expect(getImpressum).toHaveAttribute('href', '/PrivaterAutomarkt7/impressum/');
+    await getImpressum.click();
+    await expect(page).toHaveURL(/.*impressum/);
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
 });
--- e2e/mobile.spec.ts
+++ e2e/mobile.spec.ts
@@ -1,51 +1,58 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Basics', () => {
-	test.use({ viewport: { width: 600, height: 900 } });
+  test.use({ viewport: { width: 600, height: 900 } });
 
-	test.beforeEach(async ({ page }) => {
-		await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
-		await page.waitForLoadState('networkidle');
-	});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
+    await page.waitForLoadState('networkidle');
+  });
 
-	test.afterEach(async ({ page }) => {
-		page.close;
-	});
+  test.afterEach(async ({ page }) => {
+    page.close;
+  });
 
-	test('Page title is correct', async ({ page }) => {
-		await expect(page).toHaveTitle('Privater Automarkt Radolfzell');
-	});
+  test('Page title is correct', async ({ page }) => {
+    await expect(page).toHaveTitle('Privater Automarkt Radolfzell');
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
+});
+
+test.describe('Back to home', () => {
+  test.use({ viewport: { width: 600, height: 900 } });
+
+  test('Go to location and test link to home', async ({ page }) => {
+    await page.goto('http://localhost:3000/PrivaterAutomarkt7/location/');
+    await page.waitForLoadState('networkidle');
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
 });
 
 test.describe('Links from index', () => {
-	test.use({ viewport: { width: 600, height: 900 } });
+  test.use({ viewport: { width: 600, height: 900 } });
 
-	test.beforeEach(async ({ page }) => {
-		await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
-		await page.waitForLoadState('networkidle');
-	});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/PrivaterAutomarkt7/');
+    await page.waitForLoadState('networkidle');
+  });
+  test('gallery mobile', async ({ page }) => {
+    await page.click("//div[@class='dropdown']");
+    await page.getByRole('link', { name: 'Galerie' }).click();
+    await expect(page).toHaveURL(/.*gallery/);
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
 
-	test('Check link/navigation for impressum', async ({ page }) => {
-		page.hover("//div[@class='dropdown']//label[1]");
-		await page
-			.locator('div')
-			.filter({ hasText: 'Fotogalerie Standort Impressum' })
-			.locator('label')
-			.click();
-		await page.getByRole('link', { name: 'Impressum' }).click();
-		await expect(page).toHaveURL(/.*impressum/);
-	});
-	// test('Check link/navigation for location', async ({ page }) => {
-	// 	const getLocation = page.getByRole('link', { name: 'Standort' });
-	// 	await expect(getLocation).toHaveAttribute('href', '/PrivaterAutomarkt7/location/');
-	// 	await getLocation.click();
-	// 	await expect(page).toHaveURL(/.*location/);
-	// 	//await page.screenshot({ path: 'location.png', fullPage: true })
-	// });
-	// test('Check link/navigation for gallery', async ({ page }) => {
-	// 	const getGallery = page.getByRole('link', { name: 'Galerie' });
-	// 	await expect(getGallery).toHaveAttribute('href', '/PrivaterAutomarkt7/gallery/');
-	// 	await getGallery.click();
-	// 	await expect(page).toHaveURL(/.*gallery/);
-	// });
+  test('location mobile', async ({ page }) => {
+    await page.click("//div[@class='dropdown']");
+    await page.getByRole('link', { name: 'Standort' }).click();
+    await expect(page).toHaveURL(/.*location/);
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
+
+  test('impressum mobile', async ({ page }) => {
+    await page.click("//div[@class='dropdown']");
+    await page.getByRole('link', { name: 'Impressum' }).click();
+    await expect(page).toHaveURL(/.*impressum/);
+    await expect(page).toHaveScreenshot({ animations: 'disabled', fullPage: true });
+  });
 });
